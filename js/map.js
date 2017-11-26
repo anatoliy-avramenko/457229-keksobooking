9e0c1bd8e0d91f4342411ca5795ed6297570d22e
anatollko <anatollko91@gmail.com>
2017-11-27 00:10:47 +0200

    внедряет функции для генерации объявлений (avatar, title)

---
+++ js/map.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+
+// сгенерировать индексы аватаров
+var avatarIndexes = [];
+var generateAvatarIndexes = function (length) {
+  for (var i = 0; i < length; i++) {
+    avatarIndexes[i] = '0' + (i + 1);
+  }
+  return avatarIndexes;
+};
+
+
+// сгенерировать случайное число в диапазоне bottom - top
+var randomizeNumber = function (bottom, top) {
+  return Math.floor((Math.random() * top) + bottom);
+};
+
+
+// выбрать случайное уникальное наименование из массива
+var pickArrayItem = function (array) {
+  var currentIndex = randomizeNumber(0, array.length);
+  var arrayItem = array[currentIndex];
+  array.splice(currentIndex, 1);
+  return arrayItem;
+};
+
+
+// сгенерировать уникальный путь к аватару
+generateAvatarIndexes(8);
+var generateAvatarPath = function () {
+  return 'img/avatars/user' + pickArrayItem(avatarIndexes) + '.png';
+};
+
+// сгенерировать массив из 8 объявлений
+var ads = [];
+for (var i = 0; i < 8; i++) {
+  ads.push({
+    'author': {
+      'avatar': generateAvatarPath()
+    },
+
+    'offer': {
+      'title': pickArrayItem(OFFER_TITLES),
+      'address':
+    }
+  });
+}
+
+
+

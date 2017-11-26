03fdffe40176fd37750286e33a97446254dfd416
anatollko <anatollko91@gmail.com>
2017-11-27 01:26:17 +0200

    завершает функционал, создающий массив из 8ми объявлений

--- js/map.js
+++ js/map.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var OFFER_TYPE = ['flat', 'house', 'bungalo'];
+var OFFER_CHECK_TIME = ['12:00', '13:00', '14:00'];
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 // сгенерировать индексы аватаров
 var avatarIndexes = [];
@@ -27,15 +30,45 @@ var pickArrayItem = function (array) {
 };
 
 
+// выбрать случайное НЕ уникальное наименование из массива
+var pickAnyArrayItem = function (array) {
+  var currentIndex = randomizeNumber(0, array.length);
+  return array[currentIndex];
+};
+
+
+// перемешать массив и задать рандомную длину
+var cutArrayRandomly = function (array) {
+  var clonedItems = array.slice(0);
+  var shuffledItems = [];
+  var arrayLength = randomizeNumber(1, clonedItems.length);
+  for (var i = 0; i < arrayLength; i++) {
+    shuffledItems.push(pickArrayItem(clonedItems));
+  }
+  return shuffledItems;
+};
+
+
 // сгенерировать уникальный путь к аватару
 generateAvatarIndexes(8);
 var generateAvatarPath = function () {
   return 'img/avatars/user' + pickArrayItem(avatarIndexes) + '.png';
 };
 
+
+// сгенерировать координаты X и Y
+var adsCoordinates = [];
+var generateCoordinates = function () {
+  adsCoordinates[0] = randomizeNumber(300, 900);
+  adsCoordinates[1] = randomizeNumber(100, 500);
+  return adsCoordinates;
+};
+
+
 // сгенерировать массив из 8 объявлений
 var ads = [];
 for (var i = 0; i < 8; i++) {
+  generateCoordinates();
   ads.push({
     'author': {
       'avatar': generateAvatarPath()
@@ -43,10 +76,24 @@ for (var i = 0; i < 8; i++) {
 
     'offer': {
       'title': pickArrayItem(OFFER_TITLES),
-      'address':
+      'address': adsCoordinates[0] + ', ' + adsCoordinates[1],
+      'price': randomizeNumber(1000, 1000000),
+      'type': pickAnyArrayItem(OFFER_TYPE),
+      'rooms': randomizeNumber(1, 5),
+      'guests': randomizeNumber(1, 7),
+      'checkin': pickAnyArrayItem(OFFER_CHECK_TIME),
+      'checkout': pickAnyArrayItem(OFFER_CHECK_TIME),
+      'features': cutArrayRandomly(OFFER_FEATURES),
+      'description': '',
+      'photos': []
+    },
+
+    'location': {
+      'x': adsCoordinates[0],
+      'y': adsCoordinates[1]
     }
   });
 }
 
-
+console.log(ads);
 
